74b9480c89a413569a0f7be4aae0aa37554d5810
Jam Risser <jam@codejam.ninja>
2020-10-29 19:56:18 +0000

    Wired up connection postgres

--- src/controllers/externalDatabase.ts
+++ src/controllers/externalDatabase.ts
@@ -1,3 +1,15 @@
+import * as k8s from '@kubernetes/client-node';
 import Controller from './controller';
 
-export default abstract class ExternalDatabase extends Controller {}
+export default abstract class ExternalDatabase extends Controller {
+  customObjectsApi: k8s.CustomObjectsApi;
+
+  kubeConfig: k8s.KubeConfig;
+
+  constructor() {
+    super();
+    this.kubeConfig = new k8s.KubeConfig();
+    this.kubeConfig.loadFromDefault();
+    this.customObjectsApi = this.kubeConfig.makeApiClient(k8s.CustomObjectsApi);
+  }
+}
--- src/controllers/externalPostgres.ts
+++ src/controllers/externalPostgres.ts
@@ -1,15 +1,42 @@
 import { ResourceMeta } from '@dot-i/k8s-operator';
 import { Postgres } from '~/databases';
 import ExternalDatabase from './externalDatabase';
-import { ExternalPostgresResource } from '~/types';
+import { ExternalPostgresResource, ConnectionPostgresResource } from '~/types';
+import ExternaldbOperator, {
+  ResourceGroup,
+  ResourceKind,
+  ResourceVersion
+} from '~/externaldbOperator';
 
 export default class ExternalPostgres extends ExternalDatabase {
   async added(
     resource: ExternalPostgresResource,
     _meta: ResourceMeta
   ): Promise<any> {
-    if (!resource.spec?.name) return;
-    const postgres = new Postgres({});
+    if (
+      !resource.spec?.name ||
+      !resource.spec.connection?.name ||
+      !resource.spec.connection.namespace
+    ) {
+      return;
+    }
+    const connectionPostgres = (
+      await this.customObjectsApi.getNamespacedCustomObject(
+        ExternaldbOperator.resource2Group(ResourceGroup.Externaldb),
+        ResourceVersion.V1alpha1,
+        resource.spec.connection.namespace,
+        ExternaldbOperator.kind2plural(ResourceKind.ConnectionPostgres),
+        resource.spec.connection.name
+      )
+    ).body as ConnectionPostgresResource;
+    const postgres = new Postgres({
+      connectionString: connectionPostgres.spec?.url,
+      database: connectionPostgres.spec?.database,
+      host: connectionPostgres.spec?.hostname,
+      password: connectionPostgres.spec?.password,
+      port: connectionPostgres.spec?.port,
+      user: connectionPostgres.spec?.username
+    });
     await postgres.createDatabase(resource.spec.name);
   }
 }
